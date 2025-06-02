2fffe729c3c7db4f5caaa89ff4f0baf50cade26a
TheUltimatePro99 <prashilshah2008@gmail.com>
2025-06-02 12:36:34 -0700

    6/2/2025
    6:50 Video two - added basic ray casting and an environment

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField]
+    private float distance = 3f;
+    [SerializeField] private LayerMask mask;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = GetComponent<PlayerLook>().cam;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * distance);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo, distance, mask ))
+        {
+
+        }
+    }
+}
